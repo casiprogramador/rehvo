fc2e314808f1a71bfb528fa5ce3e2c1ab5b1ee79
Marcelo Choque <casiprogramador@gmail.com>
2017-10-16 23:53:17 -0400

    Inicio del proyecto

---
+++ README.md
@@ -0,0 +1,3 @@
+## Sistema RRHH REHVO
+
+Sistema de recursos humanos para el ministerio de desarrollo productivo y economia plural en Bolivia
---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
